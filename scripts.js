0bbb2e1006d6f7110f3f036b146ce49cb3f539f6
Magda <mjszewczyk91@gmail.com>
2018-03-19 20:00:03 +0100

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
 function Phone(brand, price, color, year) {
     this.brand = brand;
     this.price = price;
-    this.color = color;
-    this.yearOrProduction = year;
+    this.color = color || 'white';
+    if (year < 1970) {
+        year = 2018;
+    }
+    this.yearOfProduction = year;
 }
 Phone.prototype.printInfo = function () {
-    console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', the price is ' + this.price + ' and year of production ' + this.yearOrProduction);
+    console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', the price is ' + this.price + ' and year of production ' + this.yearOfProduction);
 }
 Phone.prototype.printYear = function () {
-    console.log('year of production: ' + this.yearOrProduction);
+    console.log('year of production: ' + this.yearOfProduction);
 }
 var iphone = new Phone('Iphone S8', 4000, 'gold', 2016);
 iphone.printInfo();
-var samsung = new Phone('Samsung Galaxy S9', 5000, 'silver', 2018);
+var samsung = new Phone('Samsung Galaxy S9', 5000, '', 2018);
 samsung.printInfo();
-var nokia = new Phone('Nokia 3310', 100, 'blue', 2000);
+var nokia = new Phone('Nokia 3310', 100, 'blue', 500);
 nokia.printInfo();
 nokia.printYear();
